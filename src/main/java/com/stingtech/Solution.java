081b039721f50f9a210557315b063f75b970a0e7
Hastings Sibanda <hastingssbnd5@gmail.com>
2024-05-14 07:21:25 +0200

    add palindrome solution to main

--- src/main/java/com/stingtech/Solution.java
+++ src/main/java/com/stingtech/Solution.java
@@ -20,4 +20,16 @@ public class Solution {
         }
         return twoNums;
     }
+    public boolean isPalindrome(int x) {
+        //first convert x to a String
+        String x_string = String.valueOf(x);
+
+        //loop and compare characters in string
+        for (int i = 0; i < ((x_string.length()) / 2); i++){
+            if (x_string.charAt(i) != (x_string.charAt(x_string.length()-1-i))){
+                return false;
+            }
+        }
+        return true;
+    }
 }
